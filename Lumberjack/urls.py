8ba9ffa2c9bdb7b19aa59738f9622d32a744ee40
Ed <edchapman88@gmail.com>
2023-06-25 14:30:56 +0100

    Set admin site title and header to Lumberjack

--- Lumberjack/urls.py
+++ Lumberjack/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 
+admin.site.site_title = "Lumberjack"
+admin.site.site_header = "Lumberjack"
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     re_path(r"^api-auth/", include("rest_framework.urls")),
