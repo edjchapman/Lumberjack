3a6f2b6a9ea1fac4d1d1d70cab17b74e1696ab9a
Ed <edchapman88@gmail.com>
2023-06-26 23:21:37 +0100

    Point default local redis host to docker service

--- backend/Lumberjack/partial_settings/celery.py
+++ backend/Lumberjack/partial_settings/celery.py
@@ -1,7 +1,7 @@
 import os
 
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER", "redis://localhost:6379")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_BROKER", "redis://localhost:6379")
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER", "redis://redis:6379")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_BROKER", "redis://redis:6379")
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
--- backend/lumberjack/partial_settings/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER", "redis://redis:6379")
-CELERY_RESULT_BACKEND = os.getenv("CELERY_BROKER", "redis://redis:6379")
-CELERY_ACCEPT_CONTENT = ["application/json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_TIMEZONE = "Europe/London"
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
